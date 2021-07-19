2f539912f3b93da4bc63db1d12ce6341849a44bb
Jordan Kittle <shwackd@gmail.com>
2021-07-19 03:39:25 -0700

    Start styling received page with pure CSS movie ticket

--- src/routes/received.svelte
+++ src/routes/received.svelte
@@ -1,35 +1,72 @@
 <script>
   let to;
   let send;
-  let btc = () => {};
-  let received, unit, asset;
+  let btc = (amt) => amt;
+  let received = .0002, unit = 'BTC', asset;
+  let movieTitle = "Silhouettes";
+  const fullDateOptions = { dateStyle: 'long', timeStyle: 'short' };
+  const shortDateOptions = { dateStyle: 'short', timeStyle: 'short' };
+  let date = new Date().toLocaleString('en-US', fullDateOptions);
+  let shortDate = new Date().toLocaleString('en-US', shortDateOptions);
 </script>
 
-	<p>
-		Received {btc(received)}
-		{unit}!
-	</p>
-
 	<div class="container">
-		<h2>Your ticket</h2>
-		<div
-			color="mb"
-			style="border: 1px dashed black; width: 600px; margin: 0 auto; padding: 20px; text-align: left;"
-		>
-			<div><b>Type:</b> Early bird (57 / 1000)</div>
-			{#if asset}
-				<div>
-					<b>Asset ID:</b>
-					<a href={`http://localhost:5005/asset/${asset}`}>{asset.substr(0, 20)}....</a>
-				</div>
-			{/if}
-			<div><b>Date:</b> June 11, 2021</div>
-		</div>
+		<p>
+			Received {btc(received)}
+			{unit}!
+		</p>
 	</div>
 
-	<div style="width: 600px; margin: 0 auto;" class="container">
+	<div class="container">
+		<!-- <h3>Your ticket</h3> -->
+		<div class="container ticket">
+			<div class="container column ticket-left grow">
+				<div class="container ticket-details">
+					<div class="ticket-prop">
+						Title:
+					</div>
+					<div class="ticket-value grow">
+						{movieTitle}
+					</div>
+					<div class="ticket-type ticket-value">
+						Early bird (57/100)
+					</div>
+				</div>
+				{#if asset}
+					<div>
+						Asset ID:
+						<a href={`http://localhost:5005/asset/${asset}`}>{asset.substr(0, 20)}....</a>
+					</div>
+				{/if}
+				<div class="container">
+					<div class="ticket-prop">
+						Date:
+					</div>
+					<div class="ticket-value grow">
+						{date}
+					</div>
+				</div>
+			</div>
+			<div class="container column ticket-stub">
+				<div>
+					{movieTitle}
+				</div>
+				<div class="ticket-value">
+					{shortDate}
+				</div>
+			</div>
+			<div id="ticket-overlay" class="overlay">
+				Admit one
+			</div>
+			<div id="stub-overlay" class="overlay">
+				1
+			</div>
+		</div>
+		
+	</div>
+
+	<!-- <div class="container">
 		<video
-			style="width: 100%; object-fit: cover"
 			muted
 			playsinline
 			autoplay
@@ -44,8 +81,8 @@
 	<div class="container">
 		<h2>Extra NFT goodies</h2>
 
-		<div style="width: 600px; margin: 0 auto;">
-			<video style="width: 100%; object-fit: cover" muted playsinline autoplay loop>
+		<div>
+			<video  muted playsinline autoplay loop>
 				<source src={'/static/girl.mp4'} />
 				Your browser does not support HTML5 video.
 			</video>
@@ -55,7 +92,118 @@
 	<div class="container">
 		<h2>Withdraw to Liquid Address</h2>
 		<div>
-			<input bind:value={to} style="width: 500px" />
+			<input bind:value={to} />
 		</div>
 		<button on:click={send}>Withdraw</button>
-	</div>
+	</div> -->
+
+	<style>
+		.ticket {
+			border: .2em dashed white;
+			width: 75%;
+			height: calc(75vw * .3);
+			position: relative;
+			padding: 1em 2em;;
+			background-color: #695a0f45;
+			color: #00000085;
+			font-size: 1.75vw;
+		}
+		.ticket-left {
+			justify-content: space-between;
+		}
+		.ticket-type {
+			white-space: nowrap;
+			padding: 0 1em;
+		}
+		.ticket-stub {
+			border-left: .1em dashed #3c3c3c40;
+			padding-left: 2em;
+			text-align: center;
+			justify-content: space-between;
+		}
+		.overlay {
+			position: absolute;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			top: 50%;
+			transform: translateY(-50%);
+			font-size: 7em;
+			color: #ffffff47;
+			font-family: 'Abril Fatface';
+		}
+		#ticket-overlay {
+			left: .3em;
+			font-size: 5em;
+			text-transform: uppercase;
+			font-family: 'Zen Tokyo Zoo';
+	
+		}
+		#stub-overlay {
+			right: .5em;	
+		}
+		.ticket-left:before {
+			content: '';
+			position: absolute;
+			display: block;
+			height: 2em;
+			width: 2em;
+			left: -1em;
+			top: -1em;
+			border-radius: 50%;
+			background-color: white;
+		}
+		.ticket-left:after {
+			content: '';
+			position: absolute;
+			display: block;
+			height: 2em;
+			width: 2em;
+			left: -1em;
+			bottom: -1em;
+			border-radius: 50%;
+			background-color: white;
+		}
+		.ticket-stub:before {
+			content: '';
+			position: absolute;
+			display: block;
+			height: 2em;
+			width: 2em;
+			right: -1em;
+			top: -1em;
+			border-radius: 50%;
+			background-color: white;
+		}
+		.ticket-stub:after {
+			content: '';
+			position: absolute;
+			display: block;
+			height: 2em;
+			width: 2em;
+			right: -1em;
+			bottom: -1em;
+			border-radius: 50%;
+			background-color: white;
+		}
+		.ticket-prop {
+			font-weight: 600;
+			width: 4em;
+		}
+		@media screen and (max-width: 769px) {
+			.ticket {
+				font-size: 2vw;
+			}
+			#ticket-overlay {
+				font-size: 4em;
+		}
+		}
+		@media screen and (max-width: 481px) {
+			.ticket {
+				font-size: 3vw;
+			}
+			#ticket-overlay {
+			font-size: 2em;
+		}
+		}
+	</style>
