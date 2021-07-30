3a1a5a477153a8e2adeeb2fb03826eedd20ba791
Jordan Kittle <shwackd@gmail.com>
2021-07-30 13:17:59 -0700

    Style verify page

--- src/routes/verify.svelte
+++ src/routes/verify.svelte
@@ -14,17 +14,38 @@
 	let back = () => go('/received');
 </script>
 
-<h2 class="container">What was word #7?</h2>
 
-<form on:submit|preventDefault={verify}>
-	<div class="form-field">
-		<div class="container" style="max-width: 200px; margin: 0 auto">
-			<input id="word" bind:value={word} autofocus autocapitalize="off" class="grow" use:focus />
+<div classs="container column">
+	<div class="container">
+		<h2 class="container">What was word #7?</h2>
+	</div>
+</div>
+<div class="container">
+	<form on:submit|preventDefault={verify} class="small-form white-form">
+		<div class="form-field">
+			<div class="container">
+				<input id="word" bind:value={word} autofocus autocapitalize="off" use:focus />
+			</div>
 		</div>
-	</div>
+	
+		<div class="container submit-controls mb">
+			<button type="button" on:click={back}>Back</button>
+			<button>Verify</button>
+		</div>
+	</form>
+</div>
 
-	<div class="container mb">
-		<button type="button" on:click={back}>Back</button>
-		<button>Verify</button>
-	</div>
-</form>
+<style>
+	#word {
+		width: 10em;
+	}
+	.submit-controls {
+		justify-content: space-between;
+	}
+	@media screen and (max-width: 769px) {
+		.submit-controls {
+			justify-content: space-evenly;
+		}
+	}
+</style>
+
