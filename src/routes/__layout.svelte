f6b71658af4b01908b00167f03b41b79d2364f7c
Adam Soltys <asoltys@gmail.com>
2021-07-30 20:56:45 -0700

    move about page to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -41,7 +41,7 @@
 				}
 			});
 
-      $player.getPlugin('click_to_pause').disable()
+			$player.getPlugin('click_to_pause').disable();
 
 			p2pml.hlsjs.initClapprPlayer($player);
 		} else {
@@ -154,6 +154,90 @@
 						<Form />
 					</div>
 				</div>
+				<div class="container wwwwwh">
+					<h1 class="mb" style="line-height: 1em">THE “WHO, WHAT, WHAT, WHERE, WHY &amp; HOW”</h1>
+
+					<h2 class="mb">WHO</h2>
+
+					<h3>SAMSON MOW</h3>
+					<p>
+						Samson Mow is Blockstream’s CSO and Pixelmatic’s CEO. Blockstream is the leading
+						provider of blockchain technologies, on the forefront of work in cryptography and
+						distributed systems. Samson founded Pixelmatic in 2011 to create engaging games that are
+						truly social and encourage new connections to be made.
+					</p>
+
+					<h3>MATT HARTLEY</h3>
+
+					<p>
+						Matt Hartley is a film director and producer; who has made numerous films and worked
+						with studios to independent production companies. Matt is the Founder of ALBA Films, a
+						new production company that produces content exclusively for the crypto space using
+						blockchain technologies.
+					</p>
+
+					<h3>ADAM SOLTYS</h3>
+
+					<p>
+						CTO of coinos, an open source bitcoin web wallet and exchange platform, and creator of
+						Raretoshi, an art marketplace for digital patronage that brings NFTs to Bitcoin and the
+						Liquid Network.
+					</p>
+
+					<h3>SHIV RAJAGOPAL</h3>
+
+					<p>
+						Shiv Rajagopal is a producer and filmmaker From Hong Kong. Having worked in the films
+						industry, he now resides in Hong Kong, working as a content writer. He is also a
+						co-founder of ALBA.
+					</p>
+
+					<h2>WHAT</h2>
+
+					<p>
+						SILHOUETTES is a sci-fi feature film that is about astronaut Katherine Barnes, who is
+						visited by an extraterrestrial, who takes her on a tour of the universe- past, present,
+						and future. It will be the first feature film to be released through this state of the
+						art streaming platform called ALBA. This will be the first of many future exclusive
+						films and series that will be distributed through this platform. Based on their time of
+						purchase, users will also be given tokenized assets and digital goodies along with
+						access to the SILHOUETTES. These NFT’s will be released as part of three separate tiers.
+					</p>
+
+					<ul>
+						<li>Tier 1 = First 100 buyers - 3 DIGITAL ASSETS/NFTs</li>
+						<li>Tier 2 = Buyer 101 to 1,101 (1000 buyers) - 2 DIGITAL ASSETS/NFTs</li>
+						<li>Tier 3 = 1102 user onwards - 1 DIGITAL ASSET/NFT</li>
+					</ul>
+
+					<h2>WHEN</h2>
+
+					<p>SILHOUETTES will be released in early August 2021.</p>
+
+					<h2>WHERE</h2>
+					<p>
+						SILHOUETTES can be accessed worldwide upon release. All you need is a digital wallet.
+					</p>
+					<h2>WHY</h2>
+					<p>
+						Consumption and creation of art has been booming with the recent NFT craze. While it
+						does a great job empowering artists, it also presents a high barrier to entry with high
+						costs and fees. Movies and TV series are an art form for the masses, and our goal is to
+						price our content as such. Through tokenization, we want to change how content is
+						experienced in the crypto space. It also brings us one step closer to
+						hyperbitcoinization. With ALBA, We aim to be the Netflix for the BTC world.
+					</p>
+
+					<h2>HOW</h2>
+
+					<p>
+						We plan to roll out SILHOUETTES and many other future ventures through our state of the
+						art platform - ALBA. You will be buying a token that represents a movie ticket to our
+						film. Without a token, you won’t be able to experience the movie. You can buy the ticket
+						using BTC, BTC Liquid, Lightning and Litecoin. The token comes with access to digital
+						goodies and tokenized assets.
+					</p>
+				</div>
 			{/if}
 		</div>
 	</section>
@@ -162,8 +246,9 @@
 <footer>
 	<div class="container column page-block">
 		<div id="social-media" class="container">
-			<FacebookIcon />
-			<TwitterIcon />
+			<a href="https://twitter.com/silhouettesnft">
+				<TwitterIcon />
+			</a>
 		</div>
 		<div class="container">
 			<p>© {year} Silhouettes Movie</p>
@@ -172,6 +257,10 @@
 </footer>
 
 <style global>
+  input,textarea {
+    padding: 1.25em 1.75em;
+  }
+
 	input,
 	label {
 		border-bottom: 1px solid var(--main-blue);
@@ -210,5 +299,20 @@
 	#sound-toggle,
 	#video-overlay {
 		z-index: 9999;
+	}
+
+	.wwwwwh {
+		display: block;
+		max-width: 1024px;
+		margin: auto;
+		padding: 4em;
+		line-height: 1.5em;
+	}
+
+	.wwwwwh h2 {
+		margin-top: 2em;
+	}
+	.wwwwwh h3 {
+		margin-top: 2em;
 	}
 </style>
