0d6b270db10dfd9f921bb3fb81fa93e739fa58c6
Adam Soltys <asoltys@gmail.com>
2021-08-10 11:52:30 -0700

    typo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -157,7 +157,7 @@
 					<VolumeIconUp />
 				{/if}
 			</div>
-			{#if $fulll && !$ticket}
+			{#if $full && !$ticket}
 				<div id="ticket-warning">Ticket not found, deposit a ticket to continue watching</div>
 			{/if}
 			<div
