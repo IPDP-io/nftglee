970b6bf25ad5f36d0965b3347cadd446f8c7a05b
Adam Soltys <asoltys@gmail.com>
2021-08-09 22:58:11 -0700

    update about page

--- src/components/about.svelte
+++ src/components/about.svelte
@@ -1,11 +1,11 @@
 <div class="container wwwwwh">
-	<h2 class="mb" style="line-height: 1em">THE “WHO, WHAT, WHEN, WHERE, WHY &amp; HOW”</h2>
+	<h2 class="mb" style="line-height: 1em">WHO? WHAT? WHEN? WHERE? WHY? HOW?</h2>
 
 	<h2 class="mb">WHO</h2>
 
 	<h3>SAMSON MOW</h3>
 	<p>
-		Samson Mow is Blockstream’s CSO and Pixelmatic’s CEO. Blockstream is the leading provider of
+		Samson Mow is CSO of Blockstream and CEO of Pixelmatic. Blockstream is the leading provider of
 		blockchain technologies, on the forefront of work in cryptography and distributed systems.
 		Samson founded Pixelmatic in 2011 to create engaging games that are truly social and encourage
 		new connections to be made.
@@ -15,66 +15,71 @@
 
 	<p>
 		Matt Hartley is a film director and producer; who has made numerous films and worked with
-		studios to independent production companies. Matt is the Founder of ALBA Films, a new production
-		company that produces content exclusively for the crypto space using blockchain technologies.
+		studios to independent production companies. He is also founder of Castle in the Sky
+		Productions, which has produced three feature films to date and are currently available on
+		Amazon Prime, iTunes, Hulu, Netflix and more.
 	</p>
 
 	<h3>ADAM SOLTYS</h3>
 
 	<p>
-		Adam Soltys is the CTO of coinos, an open source bitcoin web wallet and exchange platform, and
-		creator of Raretoshi, an art marketplace for digital patronage that brings NFTs to Bitcoin and
-		the Liquid Network.
+		Adam Soltys is the CTO of Coinos, an open source bitcoin web wallet and exchange platform. He is
+		also the creator of Raretoshi, an art marketplace for digital patronage that brings NFTs to
+		Bitcoin and the Liquid Network.
 	</p>
 
 	<h3>SHIV RAJAGOPAL</h3>
 
 	<p>
-		Shiv Rajagopal is a producer and filmmaker From Hong Kong. Having worked in the films industry,
-		he now resides in Hong Kong, working as a content writer. He is also a co-founder of ALBA.
+		Shiv Rajagopal is a producer and filmmaker from Hong Kong. Having studied in Southern
+		California, he now works in Hong Kong as a content writer and producer.
 	</p>
 
 	<h2>WHAT</h2>
 
 	<p>
-		SILHOUETTES is a sci-fi feature film that is about astronaut Katherine Barnes, who is visited by
-		an extraterrestrial, who takes her on a tour of the universe- past, present, and future. It will
-		be the first feature film to be released through this state of the art streaming platform called
-		ALBA. This will be the first of many future exclusive films and series that will be distributed
-		through this platform. Based on their time of purchase, users will also be given tokenized
-		assets and digital goodies along with access to the SILHOUETTES. These NFT’s will be released as
-		part of three separate tiers.
+		<b>SILHOUETTES</b> is a sci-fi feature film about an astronaut named Katherine Barnes, who is
+		visited by an extra-terrestrial that takes her on a tour of the universe- past, present, and
+		future. <b>SILHOUETTES</b>
+		will be the first feature film to sell tickets and access via NFTs. Upon purchase, viewers will also
+		unlock exclusive digital assets along with access to the film. These NFTs will be released in three
+		separate tiers:
 	</p>
 
 	<ul>
 		<li>Tier 1 = First 100 buyers - 3 DIGITAL ASSETS/NFTs</li>
-		<li>Tier 2 = Buyer 101 to 1,101 (1000 buyers) - 2 DIGITAL ASSETS/NFTs</li>
-		<li>Tier 3 = 1102 user onwards - 1 DIGITAL ASSET/NFT</li>
+		<li>Tier 2 = Next 1000 buyers - 2 DIGITAL ASSETS/NFTs</li>
+		<li>Tier 3 = Everyone else - 1 DIGITAL ASSET/NFT</li>
 	</ul>
+
+	<p>
+		*Please note that all tiers will be priced at the same amount. Tiers will prioritize how early
+		you got to the party, not how much you paid to get in.
+	</p>
 
 	<h2>WHEN</h2>
 
-	<p>SILHOUETTES will be released in early August 2021.</p>
+	<p><b>SILHOUETTES</b> will be released on August 13th, 2021.</p>
 
 	<h2>WHERE</h2>
-	<p>SILHOUETTES can be accessed worldwide upon release. All you need is a digital wallet.</p>
+
+	<p><b>SILHOUETTES</b> can be accessed worldwide upon release on silhouettesthemovie.com</p>
+
 	<h2>WHY</h2>
+
 	<p>
-		Consumption and creation of art has been booming with the recent NFT craze. While it does a
-		great job empowering artists, it also presents a high barrier to entry with high costs and fees.
-		Movies and TV series are an art form for the masses, and our goal is to price our content as
-		such. Through tokenization, we want to change how content is experienced in the crypto space. It
-		also brings us one step closer to hyperbitcoinization. With ALBA, We aim to be the Netflix for
-		the BTC world.
+		Consumption and creation of art is booming with the recent NFT craze. While it does a great job
+		empowering artists, there is a massive barrier to entry with high costs and fees. Film and TV
+		are an art form of the masses, and our goal is to price our content as such. Through
+		tokenization, we want to change how content is experienced. This project bridges the gap between
+		entertainment and crypto while also taking a step closer to hyperbitcoinization.
 	</p>
 
 	<h2>HOW</h2>
 
 	<p>
-		We plan to roll out SILHOUETTES and many other future ventures through our state of the art
-		platform - ALBA. You will be buying a token that represents a movie ticket to our film. Without
-		a token, you won’t be able to experience the movie. You can buy the ticket using BTC, BTC
-		Liquid, Lightning and Litecoin. The token comes with access to digital goodies and tokenized
-		assets.
+		A wallet is needed to purchase a token, which represents a movie ticket (aka access) to our
+		film. Without a token, access to experience the movie will not be permitted. Tokens can be
+		purchased using BTC, BTC Liquid, Lightning and Litecoin.
 	</p>
 </div>
