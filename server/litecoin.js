451f58df8a577ab83d23f22693d8f3f3c9799671
Adam Soltys <asoltys@gmail.com>
2021-08-11 14:08:47 -0700

    remove log

--- server/litecoin.js
+++ server/litecoin.js
@@ -39,7 +39,6 @@ zmqRawTx.on('message', async (topic, message, sequence) => {
 });
 
 zmqRawBlock.on('message', async (topic, message, sequence) => {
-  console.log("block", message);
 	let block = Block.fromHex(message.toString('hex'));
 	block.transactions.map((tx) => {
 		let hash = reverse(tx.getHash()).toString('hex');
